bcb65e396beec0900392a6cdf520c31a046ebea2
Gurjot Kaur <gurjotkaurgk888@gmail.com>
2026-09-14 10:03:39 +0530

    refine code

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ env.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
